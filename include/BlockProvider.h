a08674f14894bad8e2900d1e95913b1777963558
Collin Baker <chbaker0@gmail.com>
2015-03-22 23:10:57 -0400

    Removed Windows-specific BlockProvider from master branch

--- include/BlockProvider.h
+++ include/BlockProvider.h
@@ -3,41 +3,6 @@
 
 #include <new>
 
-#include <windows.h>
-
-//class PageSizeChecker
-//{
-//    std::size_t pageSize;
-//
-//public:
-//    PageSizeChecker() noexcept
-//    {
-//        SYSTEM_INFO sysinfo;
-//        GetSystemInfo(&sysinfo);
-//        pageSize = sysinfo.dwPageSize;
-//    }
-//
-//    std::size_t getPageSize() const noexcept {return pageSize;}
-//};
-
-template <std::size_t BlockSizeIn = 2097152>    // 2 MiB
-struct BlockProviderWindows
-{
-    static constexpr std::size_t BlockSize = BlockSizeIn;
-
-    static void* allocateBlock(void *hint) noexcept
-    {
-        return VirtualAlloc(hint, BlockSize, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
-    }
-    static void freeBlock(void *block) noexcept
-    {
-        VirtualFree(block, 0, MEM_RELEASE);
-    }
-};
-
-//template <std::size_t BlockSizeIn>
-//PageSizeChecker BlockProviderWindows<BlockSizeIn>::pageSizeChecker;
-
 template <std::size_t BlockSizeIn = 1024>
 struct BlockProviderNewDelete
 {
